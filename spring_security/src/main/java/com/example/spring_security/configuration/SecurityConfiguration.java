ed807bd215cb6299b611c2d4db905a129113840c
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-07-12 16:56:20 +0700

    Login form

--- spring_security/src/main/java/com/example/spring_security/configuration/SecurityConfiguration.java
+++ spring_security/src/main/java/com/example/spring_security/configuration/SecurityConfiguration.java
@@ -3,11 +3,13 @@ package com.example.spring_security.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -44,5 +46,17 @@ public class SecurityConfiguration {
         userDetailsManager.setUsersByUsernameQuery("select user_id, password, is_active from members where user_id = ?");
         userDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id = ?");
         return userDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(config ->
+                {
+                    config.requestMatchers("/")
+                            .authenticated()
+                            .anyRequest()
+                            .permitAll();
+                }).formLogin(config -> config.loginPage("/loginPage").permitAll());
+        return http.build();
     }
 }
